280f9a1d6331171ec93401cbfac73881bdb002af
fbignardi <federico.bignardi@gmail.com>
2014-02-10 18:15:10 +0100

    update on Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "reactive_mongo_plugin"
-  val appVersion      = "0.0.5"
+  val appVersion      = "0.0.7"
 
   val appDependencies = Seq(
     "org.reactivemongo" %% "reactivemongo" % "0.10.0"
